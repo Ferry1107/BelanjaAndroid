e44e9da64bfe537dc566f4246da167ab3e67cb65
fery <ferryfudc5@gmail.com>
2021-01-14 17:16:06 +0700

    test

--- app/src/main/java/com/example/aplikasibelanja/MainActivity.java
+++ app/src/main/java/com/example/aplikasibelanja/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String test = "";
+        String test2 = "";
+        String test4 = "jhjkhkjh";
     }
 }
